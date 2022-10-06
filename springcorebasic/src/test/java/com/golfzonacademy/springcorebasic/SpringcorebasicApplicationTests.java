d05abacc4d33cb285e666bfc60413898fe284ff2
darkblose <darkblose@gmail.com>
2022-10-06 22:18:44 +0900

    Day01 : spring-core-basic

---
+++ springcorebasic/src/test/java/com/golfzonacademy/springcorebasic/SpringcorebasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golfzonacademy.springcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorebasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

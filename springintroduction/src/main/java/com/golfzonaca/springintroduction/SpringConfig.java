7c154172449584b152c8ea59efcc499283a11f69
darkblose <darkblose@gmail.com>
2022-10-07 16:02:26 +0900

    feature : SpringDataJPA, AOP
    Day02 : Spring Introduction

--- springintroduction/src/main/java/com/golfzonaca/springintroduction/SpringConfig.java
+++ springintroduction/src/main/java/com/golfzonaca/springintroduction/SpringConfig.java
@@ -1,36 +1,32 @@
 package com.golfzonaca.springintroduction;
 
+import com.golfzonaca.springintroduction.aop.TimeTraceAop;
 import com.golfzonaca.springintroduction.repository.*;
 import com.golfzonaca.springintroduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 // 자바 코드로 직접 스프링 빈 등록하기
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired // 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public TimeTraceAop tImeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 }

54a04594f7a4f9ca72f3b6f1d4d28e92732ecbcf
darkblose <darkblose@gmail.com>
2022-10-09 10:55:56 +0900

    set : Project initialize
    Day04 : Spring Core Basic

---
+++ springcorebasic/src/test/java/com/golfzonaca/springcorebasic/SpringcorebasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golfzonaca.springcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorebasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

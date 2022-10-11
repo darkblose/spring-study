5fd44752be67287057c685d4414d531dc3e3a7b4
darkblose <darkblose@gmail.com>
2022-10-11 09:47:21 +0900

    feature : Component Scan
    Day06 : Spring Core Basic

--- springcorebasic/src/main/java/com/golfzonaca/springcorebasic/AppConfig.java
+++ springcorebasic/src/main/java/com/golfzonaca/springcorebasic/AppConfig.java
@@ -15,32 +15,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    /**
-     * @Bean memberService -> new MemoryMemberRepository()
-     * @Bean orderService -> new MemoryMemberRepository()
-     * 이 경우, 싱글톤이 깨질까? 유지될까?
-     *  -> 같은 인스턴스가 조회된다.
-     *  따라서, 모든 인스턴스가 공유되어 사용된다.
-     *
-     *  출력 예상
-     *  call AppConfig.memberService
-     *  call AppConfig.memberRepository
-     *  call AppConfig.memberRepository
-     *  call AppConfig.orderService
-     *  call AppConfig.memberRepository
-     *
-     *  실제 출력
-     *  call AppConfig.memberService
-     *  call AppConfig.memberRepository
-     *  call AppConfig.orderService
-     *
-     *  그렇다면, @Configuration 없이 @Bean만 선언하면 어떻게 될까?
-     *  -> CGLIB로 조작되지 않은 AppConfig가 호출된다.
-     *  -> 하지만, 싱글톤이 깨진다.
-     *  -> 긱자 다른 인스턴스를 생성하여 사용한다.
-     *  -> 스프링 컨테이너가 관리하지 않는, new MemberRepository()로 생성한 것과 동일한 결과가 발생한다.
-     */
-
     @Bean
     public MemberService memberService() {
         System.out.println("call AppConfig.memberService");
--- springcorebasic/src/main/java/com/golfzonaca/springcorebasic/discount/RateDiscountPolicy.java
+++ springcorebasic/src/main/java/com/golfzonaca/springcorebasic/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.golfzonaca.springcorebasic.discount;
 
 import com.golfzonaca.springcorebasic.member.Grade;
 import com.golfzonaca.springcorebasic.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;

cd2fff236b7b73249b5a580eb7a6e078639f5756
darkblose <darkblose@gmail.com>
2022-10-12 11:37:57 +0900

    Solution : Basic Basic Solution for Unexpected Issue when Singleton wirh Prototype Day07 : Spring Core Basic

--- springcorebasic/src/test/java/com/golfzonaca/springcorebasic/scope/SingletonWithPrototypeTest1.java
+++ springcorebasic/src/test/java/com/golfzonaca/springcorebasic/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package com.golfzonaca.springcorebasic.scope;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -37,19 +38,23 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
+//        private final PrototypeBean prototypeBean; // 생성시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        ApplicationContext applicationContext;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }

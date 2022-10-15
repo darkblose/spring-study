22609884fde5f2cc769fed01a5325722737d0709
darkblose <darkblose@gmail.com>
2022-10-15 21:30:53 +0800

    feature : FrontController V1 : Apply FrontController V2 : Seperate View V3 : Add Model V4 : Simple & Useful Controller V5 : Flexible Controller 1 V5 : Flexible Controller 2 Day09 : Spring MVC Vol.1

---
+++ SpringMVCVol1/Servlet/src/main/java/com/golfzonaca/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,27 @@
+package com.golfzonaca.servlet.web.frontcontroller.v2.controller;
+
+import com.golfzonaca.servlet.domain.member.Member;
+import com.golfzonaca.servlet.domain.member.MemberRepository;
+import com.golfzonaca.servlet.web.frontcontroller.MyView;
+import com.golfzonaca.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}

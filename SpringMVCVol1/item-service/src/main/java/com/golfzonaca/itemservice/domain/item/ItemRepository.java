bd90fb7d01d4b1b54f90a0d6af3656c3c796b469
darkblose <darkblose@gmail.com>
2022-10-17 17:20:34 +0900

    feature : Item Service
    Day11 : Spring MVC Vol.1

---
+++ SpringMVCVol1/item-service/src/main/java/com/golfzonaca/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,40 @@
+package com.golfzonaca.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); //동시성 문제로, ConCurrentHashMap 사용 필요
+    private static Long sequence = 0L; //동시성 문제로, atomic 사용 필요
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}

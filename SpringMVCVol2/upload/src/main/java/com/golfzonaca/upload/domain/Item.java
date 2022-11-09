991237d464f55fbb634793f2c0fef503545aa0e2
darkblose <darkblose@gmail.com>
2022-11-09 22:28:35 +0900

    feature : File Upload & Download Day19 : Spring MVC Vol.2

---
+++ SpringMVCVol2/upload/src/main/java/com/golfzonaca/upload/domain/Item.java
@@ -0,0 +1,14 @@
+package com.golfzonaca.upload.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private UploadFile attachFile;
+    private List<UploadFile> imageFiles;
+}

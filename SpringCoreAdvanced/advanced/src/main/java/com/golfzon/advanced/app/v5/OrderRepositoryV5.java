e6ac74935cf24e95373339b6d15c19020679fe12
darkblose <darkblose@gmail.com>
2022-11-15 20:42:59 +0900

    feature : LogTrace with Design Pattern(Callback) Day25 : Spring Core Advanced

---
+++ SpringCoreAdvanced/advanced/src/main/java/com/golfzon/advanced/app/v5/OrderRepositoryV5.java
@@ -0,0 +1,34 @@
+package com.golfzon.advanced.app.v5;
+
+import com.golfzon.advanced.trace.callback.TraceTemplate;
+import com.golfzon.advanced.trace.logtrace.LogTrace;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderRepositoryV5 {
+
+    private final TraceTemplate template;
+
+    public OrderRepositoryV5(LogTrace trace) {
+        this.template = new TraceTemplate(trace);
+    }
+
+    public void save(String itemId) {
+        template.execute("OrderRepository.save()", () -> {
+            //저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+            return null;
+        });
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ SpringCoreAdvanced/advanced/src/test/java/com/golfzon/advanced/trace/strategy/code/template/Callback.java
@@ -0,0 +1,5 @@
+package com.golfzon.advanced.trace.strategy.code.template;
+
+public interface Callback {
+    void call();
+}
